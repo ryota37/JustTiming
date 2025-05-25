2529910f7b01e99f2bf9a861f0a942b58ca3b519
ryota37 <ryota.nakahama@access-company.com>
2025-05-25 10:49:45 +0900

    プロジェクト ファイルを追加します。

---
+++ JustTiming/Main.cpp
@@ -0,0 +1,24 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.15
+
+void cursorUpdate(Rect& cursor)
+{
+	cursor.x += Scene::DeltaTime() * 100;
+	cursor.draw(Palette::Black);
+}
+
+void Main()
+{
+	Scene::SetBackground(ColorF{Palette::Gray});
+
+	Rect cursor = Rect{ Arg::center(Scene::Center().x, Scene::Center().y), 20, 50 };
+
+	while (System::Update())
+	{
+		Rect{ Arg::center(Scene::Center().x, Scene::Center().y), 600, 50 }.draw(Palette::Green);
+		Rect{ Arg::center(Scene::Center().x, Scene::Center().y), 300, 50 }.draw(Palette::Yellow);
+		Rect{ Arg::center(Scene::Center().x, Scene::Center().y), 50, 50 }.draw(Palette::Red);
+
+		cursorUpdate(cursor);
+	}
+}
+
