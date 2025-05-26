c1db483d84119a42e8e84112af44c95df8b6327c
ryota37 <ryota.nakahama@access-company.com>
2025-05-27 08:48:44 +0900

    (WIP) Impl: The cursor automatically moves

--- JustTiming/Main.cpp
+++ JustTiming/Main.cpp
@@ -1,16 +1,31 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.15
 
+bool isCursorStop = false;
+
 void cursorUpdate(Rect& cursor)
 {
-	cursor.x += Scene::DeltaTime() * 100;
+	const double sin = Periodic::Triangle1_1(2s);
+	if (!isCursorStop)
+	{
+		cursor.x = 400 * sin + 400;
+	}
 	cursor.draw(Palette::Black);
+}
+
+void cursorStop()
+{
+	if (KeyEnter.down())
+	{
+		isCursorStop = !isCursorStop;
+		// Need to stop the timer of Periodic::Triangle1_1
+	}
 }
 
 void Main()
 {
 	Scene::SetBackground(ColorF{Palette::Gray});
-
 	Rect cursor = Rect{ Arg::center(Scene::Center().x, Scene::Center().y), 20, 50 };
+
 
 	while (System::Update())
 	{
@@ -19,6 +34,8 @@ void Main()
 		Rect{ Arg::center(Scene::Center().x, Scene::Center().y), 50, 50 }.draw(Palette::Red);
 
 		cursorUpdate(cursor);
+		cursorStop();
+
 	}
 }
 
